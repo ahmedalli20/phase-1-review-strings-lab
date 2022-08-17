1faa2c1b90d29e5bf5c0c586757a1bea21fd6c74
Ahmed Alli <muniirhaddadi1@gmail.com>
2022-08-17 10:41:29 +0300

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code in this file!
+var currentUser = 'Mahir';
+
+ let welcomeMessage =  currentUser + "Welcome to Flatbook, !";
+
+ let excitedWelcomeMessage = currentUser.toUpperCase() + 'WELCOME TO FLATBOOK, !';
+
+ let shortGreeting ;
+    //  const firstInitial = currentUser[0];
+    //   const restOfName = currentUser.slice(1);
+      shortGreeting=currentUser.toUpperCase([0]) + "Welcome, !"
+ 
